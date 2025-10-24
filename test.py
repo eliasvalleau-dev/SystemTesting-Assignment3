d1162c0394c4e030d1edd4c52f114d8d9ce3c9ae
Vegetabs <finnlachlanmurphy@gmail.com>
2025-10-24 20:08:33 +1100

    finished implementation of check_login

--- test.py
+++ test.py
@@ -7,13 +7,18 @@ class Test():
         self.driver = webdriver.Chrome()
         self.driver.get(url)
     
-    def check_login(self):
+    def check_login(self) -> bool:
         email_entry = self.driver.find_element(By.ID, "input-email")
         email_entry.send_keys("elias.valle@student.uts.edu.au")
         pass_entry = self.driver.find_element(By.ID, "input-password")
         pass_entry.send_keys("Loginpage@")
+        submit_btn = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/div[1]/div/div/div/div[2]/div/div/form/input")
+        submit_btn.click()
+        if self.driver.current_url == "https://ecommerce-playground.lambdatest.io/index.php?route=account/account":
+            return True
+        return False
     
-    def close(self):
+    def close(self) -> None:
         self.driver.quit()
 
 if __name__ == "__main__":
