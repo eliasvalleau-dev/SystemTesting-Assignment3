5babd1b563d11b24c6709a1baf2075869e8a84b5
eliasvalleau-dev <elias.valle.au@gmail.com>
2025-10-24 14:10:32 +1100

    changed send keys

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ driver.implicitly_wait(0.5) # wait for 0.5 seconds
 text_box = driver.find_element(by=By.NAME, value="search") # find a specific text box
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button") # find the only button on the page
 
-text_box.send_keys("hello")
+text_box.send_keys("Fatneek")
 
 
 
